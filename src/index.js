c2992d76e86fb7263621a07a613a2c4d04a5a734
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2019-02-11 21:07:43 +0300

    added `unistore` and dev dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import { createElement, render } from '/@/preact.mjs';
 import App from './app/App.mjs';
+import createStore from '/@/unistore/unistore.mjs';
+import devtools    from '/@/unistore/devtools.mjs';
+
+import { Provider } from '/@/unistore/integrations/preact.mjs';
 
 const h = createElement;
 
-render(h(App), document.body);
+let initialState = { count: 1100 };
+
+let store = !DEV ?  createStore(initialState) : devtools(createStore(initialState));
+
+
+render( h(Provider, {store}, h(App) ), document.body);
